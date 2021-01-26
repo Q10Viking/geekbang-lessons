cb000e28cf6b2b8153fba6f494daa7aba33b7707
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-01-26 21:48:59 +0800

    add all bean lazy

--- thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanInitialazationDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanInitialazationDemo.java
@@ -2,10 +2,12 @@ package org.geekbang.thinking.in.spring.bean.definition;
 
 import org.geekbang.thinking.in.spring.bean.factory.DefaultUserBean;
 import org.geekbang.thinking.in.spring.bean.factory.UserFactory;
+import org.geekbang.thinking.in.spring.ioc.overview.domain.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 @Configuration
 public class BeanInitialazationDemo {
@@ -13,13 +15,14 @@ public class BeanInitialazationDemo {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(BeanInitialazationDemo.class);
-
         context.refresh();
+        context.getBean(UserFactory.class);
 
         context.close();
     }
 
     @Bean(initMethod = "initializeMethod")
+    @Lazy
     public UserFactory userFactory(){
         return new DefaultUserBean();
     }

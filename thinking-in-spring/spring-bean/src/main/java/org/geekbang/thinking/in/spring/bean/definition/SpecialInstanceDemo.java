9c50447bfcff657de9203330a1a2c34ff9c73e03
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-01-26 16:18:49 +0800

    AutowireCapableBeanFactory

--- thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/SpecialInstanceDemo.java
+++ thinking-in-spring/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/SpecialInstanceDemo.java
@@ -2,6 +2,8 @@ package org.geekbang.thinking.in.spring.bean.definition;
 
 import org.geekbang.thinking.in.spring.bean.factory.UserFactory;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Iterator;
@@ -9,7 +11,9 @@ import java.util.ServiceLoader;
 
 public class SpecialInstanceDemo {
     public static void main(String[] args) {
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/special-instantance-context.xml");
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/special-instantance-context.xml");
+        AutowireCapableBeanFactory beanFactory = applicationContext.getAutowireCapableBeanFactory();
+
         ServiceLoader<UserFactory> userFactorysrv = beanFactory.getBean("special-service-load", ServiceLoader.class);
         display(userFactorysrv);
     }

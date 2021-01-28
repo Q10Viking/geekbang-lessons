b7022edc11143c7b328b1c4de1d03d3763e26af4
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-01-28 13:09:57 +0800

    add all

---
+++ thinking-in-spring/dependency-inject/src/main/java/org/geekbang/thinking/in/spring/inject/AnnotationDependencyInjectResolverDemo.java
@@ -0,0 +1,58 @@
+package org.geekbang.thinking.in.spring.inject;
+
+import org.geekbang.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+public class AnnotationDependencyInjectResolverDemo {
+
+    /**
+     * DependencyDescriptor ->
+     * 必须（required = true）
+     * 实时注入（eager = true)
+     * 通过类型（User.class）
+     * 字段名称（"user"）
+     * 是否首要（primary = true）
+     */
+    @Autowired
+    private User user;  //  依赖查找处理      //  superUser(primary)
+
+    //  依赖注入过程中支持的类型
+
+    @Autowired
+    private Map<String ,User> map;  //  {user=user,superUser=superUser}
+
+    @Autowired
+    private Optional<User> optionalUser;    //  optional(superUser)
+
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        //  注册该类
+        applicationContext.register(AnnotationDependencyInjectResolverDemo.class);
+
+        // 加载user userHolder bean配置
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(applicationContext);
+        String beanXmlPath = "classpath:/META-INF/dependency-lookup-context.xml";
+        reader.loadBeanDefinitions(beanXmlPath);
+
+        applicationContext.refresh();
+
+        AnnotationDependencyInjectResolverDemo bean = applicationContext.getBean(AnnotationDependencyInjectResolverDemo.class);
+        System.out.println(bean.user);
+        System.out.println(bean.map);
+        System.out.println(bean.optionalUser);
+
+        applicationContext.close();
+
+    }
+}
